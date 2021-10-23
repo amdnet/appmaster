45d18aac763ec6543fe554908b8e7de42b55e2f6
amdnet <machmudseo@gmail.com>
2021-10-23 19:28:06 +0700

    revisi routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,9 +31,9 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-// $routes->get('/', 'Home::index');
-$routes->get('/', '\Myth\Auth\Controllers\AuthController::login');
-$routes->get('/dashboard', 'Home::index');
+$routes->get('/', 'Home::index');
+// $routes->get('/', '\Myth\Auth\Controllers\AuthController::login');
+// $routes->get('/dashboard', 'Home::index');
 // $routes->get('/user/(:num)', 'User::detail/$1', ['filter' => 'role:admin']);
 
 /*
